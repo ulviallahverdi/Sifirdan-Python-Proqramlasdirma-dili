411f75fbe0783a0e3697aac12989e8bd98a4620b
ulviallahverdi <61663598+ulviallahverdi@users.noreply.github.com>
2022-04-16 10:32:39 +0400

    Add files via upload

---
+++ hell_yollar.py
@@ -0,0 +1,31 @@
+#Problem 1 Helli
+ceki = 70
+boy = 175
+beden_ceki_indeksi = ceki / boy ** 2
+beden_ceki_indeksi
+
+#Problem 2 Helli
+kilometre_geden_benzin = 12
+gedilen_yol = 225
+odemeli_mebleg = gedilen_yol * kilometre_geden_benzin
+odemeli_mebleg
+
+#Problem 4
+a = 2
+b = 3
+a,b = b,a
+
+#Problem 5
+a = 4
+b = 6
+c = a**2 + b**2
+c_son = c**2
+
+#Problem 6
+#Ayliq cixan xerclerin hesablanmasi ucun gundelik odediklerimizi hesablayaq
+gunluk_yemek = 3.5   #manat
+gunluk_transport = 2 #manat
+gunluk_atisdirmaliq = 5 #manat
+aylarin_gunu = 30
+ayliq_xerc = 30 * (gunluk_yemek+gunluk_transport+gunluk_atisdirmaliq)
+ayliq_xerc
---
+++ ilkin bilikler.py
@@ -0,0 +1,298 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 1
+1
+>>> 2
+2
+>>> 3
+3
+>>> 4
+4
+>>> 5
+5
+>>> 4
+4
+>>> 6
+6
+>>> 7
+7
+>>> 8
+8
+>>> 24
+24
+>>> 56
+56
+>>> 903038495734987539845
+903038495734987539845
+>>> 
+>>> 
+>>> 
+>>> 12
+12
+>>> 12.0
+12.0
+>>> 24.5
+24.5
+>>> 1
+1
+>>> 4
+4
+5
+>>> 
+>>> 3+4
+7
+>>> 12+56
+68
+>>> 56+566239847
+566239903
+>>> 12.2 + 25
+37.2
+>>> float
+KeyboardInterrupt
+>>> 23+@3
+SyntaxError: invalid syntax
+>>> 23+23
+46
+>>> 4.0 + 5
+9.0
+>>> toplama cixma vurma bolme
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 4+3
+7
+>>> 4-2
+2
+>>> 4*2
+8
+>>> 4/2
+2.0
+>>> 
+>>> 4/1
+4.0
+>>> 4.000000000000000000000000001
+KeyboardInterrupt
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 45/5
+9.0
+>>> 56/3
+18.666666666666668
+>>> 23/3
+7.666666666666667
+>>> 90/9
+10.0
+>>> 90/10
+9.0
+>>> 4/2.0
+2.0
+>>> 2*2.0
+4.0
+>>> x = 1
+>>> x
+1
+>>> y = 100
+>>> y
+100
+>>> 1 + 100
+101
+>>> x
+1
+>>> y
+100
+>>> x + y
+101
+>>> x
+1
+>>> y
+100
+>>> 
+>>> 
+>>> 
+>>> 
+>>> x = 25
+>>> y = 35
+>>> x /y
+0.7142857142857143
+>>> y/x
+1.4
+>>> x -y
+-10
+>>> x+y
+60
+>>> x/y
+0.7142857142857143
+>>> x * y
+875
+>>> z = 45
+>>> x
+25
+>>> y
+35
+>>> z
+45
+>>> x * y * z
+39375
+>>> x * y * 2 * 90 * 100
+15750000
+>>> x* y - 5
+870
+>>> x * (y -5)
+750
+>>> a = 4
+>>> b =3
+>>> c = a + 2*b
+>>> c
+10
+>>> toplam =a +b
+>>> toplam
+7
+>>> a
+4
+>>> b
+3
+>>> a =7
+>>> a
+7
+>>> A = 7
+>>> A
+7
+>>> a
+7
+>>> a =3
+>>> A=7
+>>> a
+3
+>>> A
+7
+>>> salam = 1
+>>> SALAM = 2
+>>> salam
+1
+>>> SALAM
+2
+>>> Salam = 1
+>>> sAlam = 2
+>>> Salam
+1
+>>> sAlam
+2
+>>> print=1
+>>> print
+1
+>>> print
+1
+>>> print()
+Traceback (most recent call last):
+  File "<pyshell#107>", line 1, in <module>
+    print()
+TypeError: 'int' object is not callable
+>>> del print
+>>> print
+<built-in function print>
+>>> 2a = 3
+SyntaxError: invalid syntax
+>>> a =1
+>>> 2a
+SyntaxError: invalid syntax
+>>> a2 =1
+>>> a2
+1
+>>> +2 = 3
+SyntaxError: cannot assign to operator
+>>> _a = 1
+>>> _a
+1
+>>> _salam = 1
+>>> _salam
+1
+>>> 34, python funksiyalari, len, print, type,
+KeyboardInterrupt
+>>> print()
+KeyboardInterrupt
+>>> pi_reqemi = 3.14
+>>> teref = 4
+>>> daire = pi_reqemi * teref
+>>> daire
+12.56
+>>> daire
+12.56
+>>> daire
+12.56
+>>> 
+>>> del daire
+>>> daire
+Traceback (most recent call last):
+  File "<pyshell#128>", line 1, in <module>
+    daire
+NameError: name 'daire' is not defined
+>>> 
+>>> 
+>>> a
+1
+>>> b
+3
+>>> x
+25
+>>> y
+35
+>>> z
+45
+>>> f
+Traceback (most recent call last):
+  File "<pyshell#136>", line 1, in <module>
+    f
+NameError: name 'f' is not defined
+>>> f = 45
+>>> f
+45
+>>> a = a + 1
+>>> a
+2
+>>> a = a+ 10
+>>> a
+12
+>>> a = 10 +2
+>>> a = b + 100
+>>> a
+103
+>>> a+=1
+KeyboardInterrupt
+>>> a
+103
+>>> a+=1
+>>> a
+104
+>>> a+=100
+>>> a
+204
+>>> a = a + 100
+>>> a
+304
+>>> b
+3
+>>> a
+304
+>>> a+=b
+>>> a
+307
+>>> a
+307
+>>> a = a -5
+>>> a
+302
+>>> a-=5
+>>> a
+297
+>>> a*=4
+>>> a
+1188
+>>> a/=4
+>>> a
+297.0
+>>> a = a * 4
+KeyboardInterrupt
+>>> 
